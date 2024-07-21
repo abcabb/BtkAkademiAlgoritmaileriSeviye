a781d3db3b43edeb37dd5c1c03ddbe78ec59675c
semi <semi1907@outlook.com>
2024-07-21 22:09:46 +0300

    DataStructures Class Library Oluşturuldu. Array.cs Class Item Eklendi. Generic Array Sınıfı Interfaceleriyle Oluşturuldu

---
+++ DataStructures/Array.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DataStructures
+{
+    public class Array<T> : IEnumerable<T>, ICloneable
+    {
+        private T[] innerList;
+        public int Count { get; private set; }
+        public int Capacity => innerList.Length;
+
+        public Array()
+        {
+            innerList = new T[2];
+            Count = 0;
+        }
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace BtkAkademiAlgoritmaileriSeviye
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("");
         }
     }
 }
