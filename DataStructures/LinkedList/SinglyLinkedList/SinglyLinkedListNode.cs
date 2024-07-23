cf71591fb28b2a5702590f43cf8b1468c27b68bc
semi <semi1907@outlook.com>
2024-07-23 13:04:17 +0300

    Singly Linked List'in Node Class'ı oluşturuldu. Ve Class Diagram oluşturuldu.

---
+++ DataStructures/LinkedList/SinglyLinkedList/SinglyLinkedListNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.LinkedList.SinglyLinkedList
+{
+    internal class SinglyLinkedListNode<T>
+    {
+        public SinglyLinkedListNode<T> next { get; set; }
+        public T Value { get; set; }
+        
+        public SinglyLinkedListNode(T value)
+        {
+               this.Value = value;
+        }
+
+        public override string ToString() => $"{Value}";
+    }
+}
