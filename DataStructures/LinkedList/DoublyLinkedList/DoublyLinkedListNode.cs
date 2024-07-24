f62d5f2d9af9bd13e657cb267c73db2c28149e6f
semi <semi1907@outlook.com>
2024-07-24 14:26:31 +0300

    DoublyLinkedListNode<T> class inşa edildi

---
+++ DataStructures/LinkedList/DoublyLinkedList/DoublyLinkedListNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.LinkedList.DoublyLinkedList
+{
+    public class DoublyLinkedListNode<T>
+    {
+        public DoublyLinkedListNode<T> prev {  get; set; }
+        public DoublyLinkedListNode<T> next {  get; set; }
+        public T Value { get; set; }
+
+        public DoublyLinkedListNode(T value)
+        {
+            Value = value;
+        }
+
+        public override string ToString() => Value.ToString();
+    }
+}
