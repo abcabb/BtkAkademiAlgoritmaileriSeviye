2ca8e7bf5c0ed9e4f546194276fb408746921313
semi <semi1907@outlook.com>
2024-07-25 11:06:23 +0300

    Stack Önemli Uygulama. Postfix !

---
+++ PostFixExample.cs
@@ -0,0 +1,65 @@
+﻿using DataStructures.Stack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtkAkademiAlgoritmaileriSeviye
+{
+    public class PostFixExample
+    {
+        private string expression;
+        DataStructures.Stack.Stack<string> S = 
+            new DataStructures.Stack.Stack<string>(StackType.Array);
+
+        private string Expression()
+        {
+            int val1, val2, answer;
+            for(int i=0; i<expression.Length; i++)
+            {
+                string c = expression.Substring(i,1);
+                if(c == "*")
+                {
+                    val1 = int.Parse(S.Pop());
+                    val2 = int.Parse(S.Pop());
+                    answer = val2 * val1;
+                    S.Push(answer.ToString());
+                }
+                else if(c == "+")
+                {
+                    val1 = int.Parse(S.Pop());
+                    val2 = int.Parse(S.Pop());
+                    answer = val2 + val1;
+                    S.Push(answer.ToString());
+                }
+                else if( c == "-")
+                {
+                    val1 = int.Parse(S.Pop());
+                    val2 = int.Parse(S.Pop());
+                    answer = val2 - val1;
+                    S.Push(answer.ToString());
+                }
+                else if(c == "/")
+                {
+                    val1 = int.Parse(S.Pop());
+                    val2 = int.Parse(S.Pop());
+                    answer = val2 / val1;
+                    S.Push(answer.ToString());
+                }
+                else
+                {
+                    S.Push(c);
+                }
+            }
+            return S.Pop();
+        }
+
+        public static string Run(string expression)
+        {
+            PostFixExample e = new PostFixExample();
+            e.expression = expression;
+            return e.Expression();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,53 +16,10 @@ namespace BtkAkademiAlgoritmaileriSeviye
     {
         static void Main(string[] args)
         {
-            var charSet = new char[] { 'a', 'b', 'c', };
-            var stack1 = new DataStructures.Stack.Stack<char>();
-            var stack2 = new DataStructures.Stack.Stack<char>(StackType.LinkedList);
-
-            foreach (char c in charSet)
-            {
-                Console.Write(c + " ");
-                stack1.Push(c);
-                stack2.Push(c);
-            }
-            
-            Console.WriteLine();
-            Console.WriteLine("\nPEEK\n");
-            Console.WriteLine($"Stack1 : {stack1.Peek()}");
-            Console.WriteLine($"Stack2 : {stack2.Peek()}");
-
-            Console.WriteLine("\nCOUNT\n");
-            Console.WriteLine($"Stack1 : {stack1.Count}");
-            Console.WriteLine($"Stack2 : {stack2.Count}");
-
-            Console.WriteLine("\nPOP\n");
-            Console.WriteLine($"Stack1 : {stack1.Pop()} is removed from the list");
-            Console.WriteLine($"Stack2 : {stack2.Pop()} is removed from the list");
-
+            string LastAnswer = PostFixExample.Run("231*+9-");
+            Console.WriteLine(LastAnswer);
 
             Console.ReadKey();
-        }
-
-        private static void DoublyLinkedListApp01()
-        {
-            var dbLinkedList = new DoublyLinkedList<int>();
-
-            dbLinkedList.AddFirst(10);
-            dbLinkedList.AddFirst(20);
-            dbLinkedList.AddFirst(30);
-            //30<->20<->10
-            dbLinkedList.AddLast(40);
-            //30<->20<->10<->40
-            dbLinkedList.AddAfter(dbLinkedList.Tail, new DoublyLinkedListNode<int>(21));
-            //30<->20<->10<->40<->21
-            dbLinkedList.AddBefore(dbLinkedList.Head, new DoublyLinkedListNode<int>(50));
-            //50<->30<->20<->10<->40<->21
-
-            foreach (var item in dbLinkedList)
-            {
-                Console.WriteLine(item);
-            }
         }
     }
 }
