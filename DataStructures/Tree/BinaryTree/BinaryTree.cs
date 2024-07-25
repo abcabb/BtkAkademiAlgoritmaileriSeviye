e119c3f8b2dd754119804364191cd6d4fe03faf9
semi <semi1907@outlook.com>
2024-07-25 17:07:56 +0300

    BinaryTree class'ına Preorder() metodu eklendi

---
+++ DataStructures/Tree/BinaryTree/BinaryTree.cs
@@ -0,0 +1,42 @@
+﻿using DataStructures.Tree.BinarySearchTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Tree.BinaryTree
+{
+    public class BinaryTree<T> where T : IComparable
+    {
+        public List<Node<T>> list { get; private set; }
+        public BinaryTree()
+        {
+            list = new List<Node<T>>();
+        }
+
+        public List<Node<T>> Inorder(Node<T> root)
+        {
+            if (root != null)
+            {
+                this.Inorder(root.left);
+                list.Add(root);
+                this.Inorder(root.right);
+            }
+            return list;
+        }
+
+        public List<Node<T>> Preorder(Node<T> root)
+        {
+            if(root != null)
+            {
+                list.Add(root);
+                this.Preorder(root.left);
+                this.Preorder(root.right);
+            }
+            return list;
+        
+        
+        }   public void ClearList() => list.Clear();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ using DataStructures.Stack;
 using DataStructures.Queue;
 using DataStructures.Tree.BinarySearchTree;
 using System.Runtime.InteropServices;
+using DataStructures.Tree.BinaryTree;
 
 namespace BtkAkademiAlgoritmaileriSeviye
 {
@@ -19,6 +20,29 @@ namespace BtkAkademiAlgoritmaileriSeviye
         static void Main(string[] args)
         {
             BST<int> tree1 = new BST<int>(new List<int>() { 22, 15, 36, 20, 3, 30, 50 });
+
+            /* 
+            var list = new BinaryTree<int>().Inorder(tree1.Root); // Bu ifade List<Node<T>> döndürür.(Inorder olarak).
+            Console.WriteLine("Tree1 Inorder olarak sıralandı :");
+
+            foreach (var item in list) Console.WriteLine(item);*/
+
+            /* YADA */
+
+            /*
+            new BinaryTree<int>().bt.Inorder(tree1.Root).ForEach(x => Console.Write(x + " ")); // Bu şekilde anlık olarak nesne oluşturup onun üzerinden ağacı düzenleyip sonucu gösteriyoruz.
+
+            new BinaryTree<int>().bt.Preorder(tree1.Root).ForEach(x => Console.Write(x + " ")); // Sonrasında çıktı alınınca nesneler kayıtlı olmadığı için yok olup gidiyorlar.
+            */
+
+            var bt = new BinaryTree<int>(); //BinaryTree class'ına ait nesne oluşuturursak
+
+            bt.Inorder(tree1.Root).ForEach(x => Console.Write(x + " ")); //Listeye Inorder olarak kaydettiği değerlerin üstüne
+            Console.WriteLine("\n");
+            bt.Preorder(tree1.Root).ForEach(x => Console.Write(x + " ")); // Bir daha listeye elemanları bu sefer Preorder olarak ekler. 
+            //Dolayısıyla aynı nesne üzerinden yapacaksak, nesneyi temizlemeye yarayan bir fonksiyon kullanmalıyız. Yani nesneyi temizlemeliyiz.
+
+            bt.ClearList(); // Şimdi liste temizlendi ve istediğin şekilde sıralamayı yazabilirsin.
 
             Console.ReadKey();
         }
