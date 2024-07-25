ae4d1a47c0f1299cf3c7ffb2a9a3c2222cf9ba8b
semi <semi1907@outlook.com>
2024-07-25 12:51:38 +0300

    ArrayQueue.cs ve LinkedListQueue.cs built.

--- DataStructures/Queue/ArrayQueue.cs
+++ DataStructures/Queue/ArrayQueue.cs
@@ -1,22 +1,38 @@
-﻿namespace DataStructures.Queue
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+
+namespace DataStructures.Queue
 {
     internal class ArrayQueue<T> : IQueue<T>
     {        
-        public int Count => throw new System.NotImplementedException();
-
-        public T Dequeue()
-        {
-            throw new System.NotImplementedException();
-        }
+        private readonly List<T> list = new List<T>();
+        public int Count { get; private set; }
 
         public void Enqueue(T item)
         {
-            throw new System.NotImplementedException();
+            if (item == null) throw new ArgumentNullException();
+
+            list.Add(item); //Listenin sonuna eleman eklendi.
+            Count++;
         }
+
+        public T Dequeue()
+        {
+            if (Count == 0) throw new Exception("Queue is empty.");
+
+            var temp = list[0];
+            list.RemoveAt(0);
+            Count--;
+            return temp;
+        }
+
 
         public T Peek()
         {
-            throw new System.NotImplementedException();
+            if (Count == 0) throw new Exception("Queue is empty.");
+            
+            return list[0];
         }
     }
 }
--- DataStructures/Queue/LinkedListQueue.cs
+++ DataStructures/Queue/LinkedListQueue.cs
@@ -1,22 +1,39 @@
-﻿namespace DataStructures.Queue
+﻿using DataStructures.LinkedList.DoublyLinkedList;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using System.Threading;
+
+namespace DataStructures.Queue
 {
     internal class LinkedListQueue<T> : IQueue<T>
     {
-        public int Count => throw new System.NotImplementedException();
-
-        public T Dequeue()
-        {
-            throw new System.NotImplementedException();
-        }
+        private readonly DoublyLinkedList<T> list = new DoublyLinkedList<T>();
+        public int Count { get; private set; }
 
         public void Enqueue(T item)
         {
-            throw new System.NotImplementedException();
+            if (item == null) throw new ArgumentNullException();
+
+            list.AddLast(item);
+            Count++;
         }
 
-        public T Peek()
+        public T Dequeue()
         {
-            throw new System.NotImplementedException();
+            if (list == null) throw new Exception("Queue is empty");
+                        
+            var temp = list.RemoveFirst();
+            Count--;
+
+            return temp;
         }
+
+        public T Peek() => Count == 0 ? throw new Exception("Queue is empty.") : list.Head.Value; // Daha kompakt bir yazım şekli.
+        /*{
+            if (list == null) throw new Exception("Queue is empty");
+            
+            return list.Head.Value;
+        }*/
     }
 }
