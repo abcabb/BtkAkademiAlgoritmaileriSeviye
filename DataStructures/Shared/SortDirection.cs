0a2d41886eade91c0696ede73e2ca1127c8fd77c
semi <semi1907@outlook.com>
2024-08-02 16:52:17 +0300

    Shared altında SortDirection.cs ve CustomComparer.cs class'ları oluşturuldu.

---
+++ DataStructures/Shared/SortDirection.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Shared
+{
+    public enum SortDirection
+    {
+        Ascending = 0,
+        Descending = 1
+    }
+}
