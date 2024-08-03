cc62640f188438a338362a36e2eb71571d41aeb8
semi <semi1907@outlook.com>
2024-08-03 13:14:17 +0300

    Graph Interfaces temeli.

---
+++ DataStructures/Graph/IGraph.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Graph
+{
+    public interface IGraph<T> : IEnumerable<T>
+    {
+
+    }
+
+    public interface IGraphVertex<T> : IEnumerable<T>
+    {
+
+    }
+
+    public interface IEdge<T>
+    {
+
+    }
+}
