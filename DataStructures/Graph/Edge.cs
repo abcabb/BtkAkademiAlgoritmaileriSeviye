fcc6aeab11a60875dd18b26c741400a2bc7771df
semi <semi1907@outlook.com>
2024-08-03 14:42:05 +0300

    AdjecencySet/Graph class'ı oluşuturuldu.

---
+++ DataStructures/Graph/Edge.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Graph
+{
+    public class Edge<T,C> : IEdge<T>  // T key, C ağırlık değerini temsil eder.
+        where C : IComparable<T>          
+    {
+        private object weight { get; }
+
+        public Edge(IGraphVertex<T> target, C weight)
+        {
+            TargetVertex = target;
+            this.weight = weight;
+        }
+
+
+        public T TargetVertexKey => TargetVertex.Key;
+
+        public IGraphVertex<T> TargetVertex { get; private set; }
+
+
+        public W Weight<W>() where W : IComparable
+        {
+            return (W)weight;
+        }
+
+        public override string ToString()
+        {
+            return TargetVertexKey.ToString();
+        }
+    }
+}
