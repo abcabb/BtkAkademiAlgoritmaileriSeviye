595d77647090b73f728a76cc3cf9adc4dc80613e
semi <semi1907@outlook.com>
2024-08-04 19:27:09 +0300

    DiEdge class ı inşa edildi.

--- DataStructures/Graph/Edge.cs
+++ DataStructures/Graph/Edge.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace DataStructures.Graph
 {
-    public class Edge<T,C> : IEdge<T>  // T key, C ağırlık değerini temsil eder.
-        where C : IComparable  
+    public class Edge<T,TW> : IEdge<T>  // T key, C ağırlık değerini temsil eder.
+        where TW : IComparable  
     {
         private object weight { get; }
         public T TargetVertexKey => TargetVertex.Key;
 
         public IGraphVertex<T> TargetVertex { get; private set; }
 
-        public Edge(IGraphVertex<T> target, C weight)
+        public Edge(IGraphVertex<T> target, TW weight)
         {
             TargetVertex = target;
             this.weight = weight;
@@ -28,6 +28,25 @@ namespace DataStructures.Graph
         public override string ToString()
         {
             return TargetVertexKey.ToString();
+        }
+    }
+
+    public class DiEdge<T, TW> : IDiEdge<T>
+    {
+        private object weight;
+        public IDiGraphVertex<T> TargetVertex { get; }
+
+        public T TargetVertexKey => TargetVertex.Key;
+
+        IGraphVertex<T> IEdge<T>.TargetVertex => TargetVertex as IGraphVertex<T>;
+
+        public W Weight<W>() where W : IComparable<T>
+        {
+            return (W)weight;
+        }
+        public override string ToString()
+        {
+            return $"{TargetVertexKey}";
         }
     }
 }
