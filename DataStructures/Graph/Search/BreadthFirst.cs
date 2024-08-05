a647dc17746ebedf1c8c15be9bd6ad19824ec6fe
semi <semi1907@outlook.com>
2024-08-05 19:36:37 +0300

    BreadthFirst class'ı inşa edildi

---
+++ DataStructures/Graph/Search/BreadthFirst.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Graph.Search
+{
+    public class BreadthFirst<T>
+    {
+        public bool Find(IGraph<T> graph, T vertexKey)
+        {
+            return bfs(graph.ReferanceVertex, new HashSet<T>(), vertexKey);
+        }
+
+        private bool bfs(IGraphVertex<T> referenceVertex, HashSet<T> visited, T searchVertexKey)
+        {
+            var Q = new DataStructures.Queue.Queue<IGraphVertex<T>>();
+            Q.Enqueue(referenceVertex);
+            visited.Add(referenceVertex.Key);
+
+            while(Q.Count > 0)
+            {
+                var current = Q.Dequeue();
+                if (current.Key.Equals(searchVertexKey))
+                    return true;
+
+                foreach (var edge in current.Edges)
+                {
+                    if (visited.Contains(edge.TargetVertexKey))
+                        continue;
+                    visited.Add(edge.TargetVertexKey);
+                    Console.WriteLine(edge.TargetVertexKey);
+                    Q.Enqueue(edge.TargetVertex);
+                }
+            }
+
+            return false;
+        }
+    }
+}
