fff926d4361096959b5ba23db011e110a8dc8a69
semi <semi1907@outlook.com>
2024-08-05 11:46:53 +0300

    DiGraph Uygulaması

--- Program.cs
+++ Program.cs
@@ -20,26 +20,36 @@ namespace BtkAkademiAlgoritmaileriSeviye
     {
         static void Main(string[] args)
         {
-            var weightedGraph = new DataStructures.Graph.AdjecencySet.WeightedGraph<char, double>(new char[] { 'A', 'B', 'C', 'D' });
+            var diGraph = new DataStructures.Graph.AdjecencySet.DiGraph<char>(new char[] { 'A', 'B', 'C', 'D', 'E' });
 
-            weightedGraph.AddEdge('A', 'B', 1.2);
-            weightedGraph.AddEdge('A', 'D', 2.3);
-            weightedGraph.AddEdge('C', 'D', 5.5);
+            diGraph.AddEdge('B', 'A');
+            diGraph.AddEdge('A', 'D');
+            diGraph.AddEdge('D', 'E');
+            diGraph.AddEdge('C', 'E');
+            diGraph.AddEdge('C', 'D');
+            diGraph.AddEdge('C', 'B');
+            diGraph.AddEdge('C', 'A');
 
-            Console.WriteLine("Is there an edge between A and B ? {0}", weightedGraph.HasEdge('A', 'B') ? "Yes" : "No");
-            Console.WriteLine("Is there an edge between A and D ? {0}", weightedGraph.HasEdge('A', 'D') ? "Yes" : "No");
-
-            foreach (var vertex in weightedGraph) // GetEnumerator() fonksiyonu Select(x=>x.Key) ile her seferinde Dictionary'nin Key değerini yani char değerini döndürecek.
+            foreach (var key in diGraph)
             {
-                Console.WriteLine(vertex + "'s relations :");
-                foreach (var key in weightedGraph.GetVertex(vertex)) // Buradaki foreach Düğümün GetEnumerator() fonksiyonunu çalıştırır.
-                    // vertex sadece key değeri oluduğu için GetVertex() ile weightedGraphVertex nesnesini aldık. GetEnumerator() çalışınca edges field'ınınn değerlerini döner.
+                Console.WriteLine(key + "'s Relations :");
+                foreach (var targetVertexKey in diGraph.GetVertex(key))
                 {
-                    var node = weightedGraph.GetVertex(key); // Target Vertex'in değeri ile target vertex nesnesine eriştik.
-                    Console.WriteLine($"{vertex} - - {key} "); /*{node.GetEdge(weightedGraph.GetVertex(vertex)).Weight<double>()}*/ //Bunu yapamadım hata alıyorum. Sonra bakacağım.
+                    Console.WriteLine($"\t{key} --> {targetVertexKey}");
                 }
             }
-            Console.WriteLine($"There is {weightedGraph.Count} vertex.");
+
+            diGraph.RemoveVertex('C');
+            Console.WriteLine("\n\nC Vertex Removed\n");
+            foreach (var key in diGraph)
+            {
+                Console.WriteLine(key + "'s Relations :");
+                foreach (var targetVertexKey in diGraph.GetVertex(key))
+                {
+                    Console.WriteLine($"\t{key} --> {targetVertexKey}");
+                }
+            }
+
             Console.ReadKey();
         }
     }
