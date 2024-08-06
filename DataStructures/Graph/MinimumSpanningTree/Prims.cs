52fef0f62288a0de94dbb989c107b9e5497ae37c
semi <semi1907@outlook.com>
2024-08-06 18:46:55 +0300

    Prims class'ı inşa edildi(Minimum Spanning Tree Prims Algorithm)

--- DataStructures/Graph/MinimumSpanningTree/Prims.cs
+++ DataStructures/Graph/MinimumSpanningTree/Prims.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataStructures.Heap;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,12 +10,13 @@ namespace DataStructures.Graph.MinimumSpanningTree
     internal class Prims <T,TW> //bunca zaman IComparable interface'ini kullanma sebebimiz, Generic ifadelerde operand'ları kullanamadıımız için CompareTo metodunun iyi bir seçenek olmasıydı.
         where T : IComparable where TW : IComparable
     {
-        /*public List<MSTEdge<T,TW>> FindMinimumSpanningTree(IGraph<T> graph)
+        public List<MSTEdge<T,TW>> FindMinimumSpanningTree(IGraph<T> graph)
         {
             var edges = new List<MSTEdge<T, TW>>();
 
-            dfs(graph, graph.ReferanceVertex,
-                new Heap.BinaryHeap<MSTEdge<T, TW>>(Shared.SortDirection.Ascending), // Egde tutan minHeap
+            dfs(graph,
+                graph.ReferanceVertex,
+                new Heap.BinaryHeap<MSTEdge<T, TW>>(Shared.SortDirection.Ascending), // minHeap ile Edge saklamak için
                 new HashSet<T>(),
                 edges); // Edge'lerin listesi
 
@@ -22,12 +24,45 @@ namespace DataStructures.Graph.MinimumSpanningTree
         }
 
         private void dfs(IGraph<T> graph, 
-            IGraphVertex<T> currentVertex,
-            BinaryHeap<MSTEdge<T,TW>>() spNeighbor,
-            HashSet<T> spVertices,
-            List<MSTEdge<T,TW>> edges)
+            IGraphVertex<T> currentVertex, 
+            BinaryHeap<MSTEdge<T, TW>> spNeighbor, 
+            HashSet<T> spVertices, 
+            List<MSTEdge<T, TW>> spEdges)
         {
+            while (true)
+            {
+                //kenarlara dikkat
+                foreach (var edge in currentVertex.Edges)
+                {
+                    //min-heap
+                    var e = new MSTEdge<T, TW>(currentVertex.Key,
+                                                edge.TargetVertexKey,
+                                                edge.Weight<TW>());
+                    spNeighbor.Add(e);
+                }
 
-        }*/
+                //Minimum Edge değeri
+                var minEdge = spNeighbor.DeleteMinMax();
+
+                //Var olan kenarları dikkate alma
+                if(spVertices.Contains(minEdge.Source) && spVertices.Contains(minEdge.Destination))
+                {
+                    minEdge = spNeighbor.DeleteMinMax();
+                    if(spNeighbor.Count == 0)
+                        return;                         //Fonksiyonun bitiş noktası. 
+                }
+
+                //vertex takibi
+                if (!spVertices.Contains(minEdge.Source))
+                {
+                    spVertices.Add(minEdge.Source);
+                }
+
+                spVertices.Add(minEdge.Destination);
+                spEdges.Add(minEdge);
+
+                currentVertex = graph.GetVertex(minEdge.Destination);
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -20,17 +20,7 @@ namespace BtkAkademiAlgoritmaileriSeviye
     {
         static void Main(string[] args)
         {
-            var heap = new DataStructures.Heap.BinaryHeap<int>(DataStructures.Shared.SortDirection.Ascending, new int[] { 54, 45, 36, 27, 29, 18, 21, 99 });
-
-            Console.WriteLine("Min Heap :");
-            foreach (int i in heap)
-                Console.Write(i + " ");
-
-            var heap2 = new DataStructures.Heap.BinaryHeap<int>(DataStructures.Shared.SortDirection.Descending, new int[] { 54, 45, 36, 27, 29, 18, 21, 99 });
-
-            Console.WriteLine("\nMax Heap :");
-            foreach (int i in heap2)
-                Console.Write(i + " ");
+            
 
             Console.ReadKey();
         }
