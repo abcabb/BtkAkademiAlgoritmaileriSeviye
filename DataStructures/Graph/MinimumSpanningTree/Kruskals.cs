721318eeb954069e162e1d2cf200af748af52aad
semi <semi1907@outlook.com>
2024-08-07 01:05:43 +0300

    Kruskals class'ı implemente edildi. (Minimum Spanning Tree Kruskal's Algorithm)

--- DataStructures/Graph/MinimumSpanningTree/Kruskals.cs
+++ DataStructures/Graph/MinimumSpanningTree/Kruskals.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,24 +13,81 @@ namespace DataStructures.Graph.MinimumSpanningTree
     {
         public List<MSTEdge<T,TW>> FindMinimumSpanningTree(IGraph<T> graph)
         {
-            // 1- Kenar Listesi (dfs)
+            // 1- Edge Listesi (dfs)
             var edges = new List<MSTEdge<T, TW>>();
             dfs(graph.ReferanceVertex,
                 new HashSet<T>(),
                 new Dictionary<T, HashSet<T>>(),
                 edges);
-            // 2- Kenar Sıralama
-            // 3- MakeSet
-            // 4- UNION
-            throw new NotImplementedException();
+            // 2- Edge Sıralama
+            var heap = new Heap.BinaryHeap<MSTEdge<T, TW>>(Shared.SortDirection.Ascending);
+            foreach (var edge in edges)
+                heap.Add(edge);                                 // Edge'leri min-heap içerisinde tuttuk.
+
+            var sortedEdgeArr = new MSTEdge<T, TW>[edges.Count];
+            for (int i = 0; i < edges.Count; i++)
+                sortedEdgeArr[i] = heap.DeleteMinMax();                   // Edge'leri küçükten büyüğe dizi içerisine kaydettik. (MSTEdge nesnesi olarak)
+
+            // 3- MakeSet - FindSet - Union
+            var disJointSet = new Set.DisjointSet<T>();
+            foreach (var vertex in graph.VerticesAsEnumerable)
+                disJointSet.MakeSet(vertex.Key);
+
+            var resultEdgeList = new List<MSTEdge<T, TW>>();
+            for(int i=0; i<edges.Count; i++)
+            {
+                var currentEdge = sortedEdgeArr[i];
+                var setA = disJointSet.FindSet(currentEdge.Source);
+                var setB = disJointSet.FindSet(currentEdge.Destination);
+
+                if (setA.Equals(setB))
+                    continue;
+
+                resultEdgeList.Add(currentEdge);
+                disJointSet.Union(setA, setB);
+            }
+
+            return resultEdgeList;
         }
 
         private void dfs(IGraphVertex<T> currentVertex, 
             HashSet<T> visitedVertices, 
-            Dictionary<T, HashSet<T>> visitedEdge,
+            Dictionary<T, HashSet<T>> visitedEdges,
             List<MSTEdge<T, TW>> edges)
         {
-            throw new NotImplementedException();
+            if (!visitedVertices.Contains(currentVertex.Key))
+            {
+                visitedVertices.Add(currentVertex.Key);
+                foreach (var edge in currentVertex.Edges)
+                {
+                    if(!visitedEdges.ContainsKey(currentVertex.Key) || !visitedEdges[currentVertex.Key].Contains(edge.TargetVertexKey)) 
+                        //Yani, visitedEdges field'ında key olarak currentVertex değeri yoksa ya da visited edges filed'ında HashSet'de current'ın targetVertexEdge key'i yoksa içeri gir.
+                    {
+                        // Edge ekleme
+                        edges.Add(new MSTEdge<T, TW>(currentVertex.Key,
+                            edge.TargetVertexKey,
+                            edge.Weight<TW>()));
+
+                        //visitedEdges güncelleme (Source için)
+                        if (!visitedEdges.ContainsKey(currentVertex.Key))
+                        {
+                            visitedEdges.Add(currentVertex.Key, new HashSet<T>());
+                        }
+                        
+                        visitedEdges[currentVertex.Key].Add(edge.TargetVertexKey);
+
+                        //visitedEdges güncelleme (Destination için) 
+                        if (!visitedEdges.ContainsKey(edge.TargetVertexKey))
+                        {
+                            visitedEdges.Add(edge.TargetVertexKey, new HashSet<T>());
+                        }
+                        
+                        visitedEdges[edge.TargetVertexKey].Add(currentVertex.Key);
+
+                        dfs(edge.TargetVertex, visitedVertices, visitedEdges, edges);
+                    }
+                }
+            }
         }
     }
 }
