3a7a364776898b405d858d07e6bf5a1c1424f2ae
semi <semi1907@outlook.com>
2024-08-06 21:04:11 +0300

    Kruskals class oluşturuldu.

---
+++ DataStructures/Graph/MinimumSpanningTree/Kruskals.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Graph.MinimumSpanningTree
+{
+    public class Kruskals<T,TW>
+        where T : IComparable
+        where TW : IComparable
+    {
+        public List<MSTEdge<T,TW>> FindMinimumSpanningTree(IGraph<T> graph)
+        {
+            // 1- Kenar Listesi (dfs)
+            var edges = new List<MSTEdge<T, TW>>();
+            dfs(graph.ReferanceVertex,
+                new HashSet<T>(),
+                new Dictionary<T, HashSet<T>>(),
+                edges);
+            // 2- Kenar Sıralama
+            // 3- MakeSet
+            // 4- UNION
+            throw new NotImplementedException();
+        }
+
+        private void dfs(IGraphVertex<T> currentVertex, 
+            HashSet<T> visitedVertices, 
+            Dictionary<T, HashSet<T>> visitedEdge,
+            List<MSTEdge<T, TW>> edges)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
