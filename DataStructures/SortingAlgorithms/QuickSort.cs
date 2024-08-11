c66eed45fdb15fd52272c4491e5cee3bada9237d
semi <semi1907@outlook.com>
2024-08-11 13:06:36 +0300

    QuickSort Algoritması inşa edildi.

--- DataStructures/SortingAlgorithms/QuickSort.cs
+++ DataStructures/SortingAlgorithms/QuickSort.cs
@@ -9,6 +9,20 @@ namespace DataStructures.SortingAlgorithms
 {
     public class QuickSort
     {
+        public static void Sort<T>(T[] array) where T : IComparable
+        {
+            Sort(array, 0, array.Length - 1);
+        }
+        private static void Sort<T>(T[] array, int lower, int  upper) where T : IComparable
+        {
+            if(lower < upper)
+            {
+                int pi = Partition<T>(array, lower, upper);
+                Sort<T>(array, lower, pi);                    // Lower(yani sıfıtr) index'ten Pivot'un Index'ine kadar olan aralığı uzatıyoruz
+                Sort<T>(array, pi+1, upper);                  // Pivot'un Index'inden Upper(en son) index'ine kadar olan aralığı uzatıyoruz.
+                // Böylece bu şekilde recursif çalışarak her seferinde pivotları yerleştire yerleştire diziyi düzenlemiş olacak.
+            }
+        }
         private static int Partition<T>(T[] array, int lower, int upper) where T : IComparable
         {
             int i = lower;
--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ namespace BtkAkademiAlgoritmaileriSeviye
         {
             var arr = new int[] { 15, 3, 8, 6, 10, 2 };
 
-            DataStructures.SortingAlgorithms.InsertionSort.Sort<int>(arr, DataStructures.Shared.SortDirection.Ascending);
+            DataStructures.SortingAlgorithms.QuickSort.Sort<int>(arr);
 
             foreach(int i in arr) 
                 Console.Write(i + " ");
